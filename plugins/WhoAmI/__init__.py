4b128df360878f5136b8d0fedb3e3d432eb653d0
cytec <iamcytec@googlemail.com>
2012-03-21 19:14:26 +0100

    update WhoAmI to match new merge requiptments

--- plugins/WhoAmI/__init__.py
+++ plugins/WhoAmI/__init__.py
@@ -13,13 +13,30 @@ from siriObjects.uiObjects import *
 from siriObjects.systemObjects import *
 from siriObjects.contactObjects import *
 
+
+res = {
+    'command': {
+        'de-DE': u'(Wer bin ich.*)|(Wie ist mein Name.*)',
+        'en-US': u'(Who am I.*)|(What\'s my name.*)'
+    },
+    'answer': {
+        'de-DE': u'Du bist {0}, das hast du mir jedenfalls gesagt.',
+        'en-US': u'You\'re {0}, that\'s what you told me. anyway.'
+    }
+}
+
+helpPhrases = {
+    'en-US': u'Who am I, Whats my name',
+    'de-DE': u'Wer bin ich, Wie ist mein Name',
+}
+
 class meCard(Plugin):
 	
-    @register("en-US", "(Who am I.*)|(What's my name.*)")
+    @register("en-US", res['command']['en-US'])
+    @register('de-DE', res['command']['de-DE'])
 	def mePerson(self, speech, language):
 		
-		self.say("You're {0}, that's what you told me. anyway.".format(self.user_name()))		
-		
+		self.say(res['answer'][language].format(self.user_name()))		
 		person_search = PersonSearch(self.refId)
 		person_search.scope = PersonSearch.ScopeLocalValue
 		person_search.me = "true"        
